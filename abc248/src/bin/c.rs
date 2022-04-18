58c07f448b7432ab8a22b720a89eba036ef1b67b
takuminmin-m <takuminmin0718@gmail.com>
2022-04-18 21:26:55 +0900

    challenge abc248/c

--- abc248/src/bin/c.rs
+++ abc248/src/bin/c.rs
@@ -1,5 +1,25 @@
 use proconio::input;
 
 fn main() {
-    
+    input! {
+        n: usize,
+        m: usize,
+        k_: usize,
+    }
+
+    let mut dp = vec![vec![0; k_+1]; n+1];
+    dp[0][0] = 1;
+
+    for i in 0..n {
+        for j in 0..k_ {
+            for k in 0..(m+1) {
+                if j + k <= k_ {
+                    dp[i+1][j+k] += dp[i][j];
+                    dp[i+1][j+k] %= 998244353;
+                }
+            }
+        }
+    }
+
+    println!("{}", dp[n].iter().sum::<usize>() % 998244353);
 }

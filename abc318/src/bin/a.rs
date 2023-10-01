a20c68970c2b36b37881f28a0c200ae1d9e26fca
takuminmin-m <takuminmin0718@gmail.com>
2023-10-01 18:28:14 +0900

    commit

--- abc318/src/bin/a.rs
+++ abc318/src/bin/a.rs
@@ -1,5 +1,13 @@
+use std::println;
+
 use proconio::{ input, marker::Chars };
 
 fn main() {
-    
+    input!{
+        n: i32,
+        m: i32,
+        p: i32,
+    }
+
+    println!("{}", (n-m+p)/p);
 }
